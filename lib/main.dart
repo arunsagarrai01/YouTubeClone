c689e55891c25d8c5a44c9c2bd1cfa5591ca4411
arunsagarrai01 <arunsagarrai6@gmail.com>
2026-08-11 19:52:30 +0545

    Border radius lear

--- lib/main.dart
+++ lib/main.dart
@@ -3,27 +3,39 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+        ),
       ),
-      home: const MyHomePage(title: 'This is my gallery.'),
+      home: const MyHomePage(
+        title: 'This is my gallery.',
+      ),
     );
   }
 }
+
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({
+    super.key,
+    required this.title,
+  });
 
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
+
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
@@ -32,34 +44,62 @@ class _MyHomePageState extends State<MyHomePage> {
       _counter++;
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor:
+        Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Container(height: 200,
-          width: 200,
-          color: Colors.red,
-        alignment: Alignment.center,
-        child: Text("Container"),
-        ),
 
-        /*Column(
-          mainAxisAlignment: .center,
-          children: [
-            const Text('These are the beautiful images.'),
-            Image.asset("assets/img.jpg"),
-            Image.asset("assets/mountain.jpg"),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: SafeArea(
+        child: Container(
+          height: 250,
+          width: 300,
+
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 10,
+          ),
+
+          margin: const EdgeInsets.all(30),
+
+          alignment: Alignment.center,
+
+          decoration: BoxDecoration(
+            color: Colors.red,
+
+            border: Border.all(
+              color: Colors.green,
+              width: 10,
             ),
-          ],
-        ),*/
+
+            // Makes the corners round
+            borderRadius: BorderRadius.circular(30),
+          ),
+
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Hello',
+              ),
+
+              const SizedBox(height: 20),
+
+              Text(
+                'Counter: $_counter',
+                style: Theme.of(context)
+                    .textTheme
+                    .headlineMedium,
+              ),
+            ],
+          ),
+        ),
       ),
+
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
